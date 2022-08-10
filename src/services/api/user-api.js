10f188fb31e165806dff4c7501d4da5c8a357b01
yplog <yalinpala@protonmail.com>
2022-08-10 12:39:53 +0300

    login

--- src/services/api/user-api.js
+++ src/services/api/user-api.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const userDetail = {
   id: 1,
   name: "Foo",
@@ -5,7 +7,19 @@ const userDetail = {
   place: "Mars"
 };
 
+function loginUser(loginData) {
+  axios.post(`/user`, loginData)
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
 function getUser(user) {
+  console.log('.env', process.env.REACT_APP_NOT_SECRET_CODE);
+
   return new Promise((resolve, reject) =>
     setTimeout(() => {
       if (user === "foo") return resolve(userDetail);

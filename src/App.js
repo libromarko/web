a81e2b8ea993f40d498075e707ec4bf57ed26530
yplog <yalinpala@protonmail.com>
2022-08-10 22:36:02 +0300

    redux typescript init

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import PublicRoutes from "./routes/public-routes";
-
-function App() {
-  return (
-    <PublicRoutes />
-  );
-}
-
-export default App;
--- src/store/authStoreSelector.js
+++
@@ -1,7 +0,0 @@
-export const userSelector = (state) => state.user;
-export const authStateSelector = (state) => ({
-  user: state.user,
-  status: state.status,
-  error: state.error,
-  login: state.login
-});

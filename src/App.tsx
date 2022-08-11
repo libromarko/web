959b43fe94272f53cbcaca98dcb75432497274d5
yplog <yalinpala@protonmail.com>
2022-08-11 15:25:49 +0300

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Loading from "./components/Loading";
 import PrivateRoute from "./routes/PrivateRoute";
 import PublicRoute from "./routes/PublicRoute";
 import { routes } from "./routes/routes";
@@ -9,31 +10,33 @@ const SignupPage = lazy(() => import("./pages/Signup/Signup"));
 const LandingPage = lazy(() => import("./pages/Landing/Landing"));
 
 function App() {
-  const isAuthenticated = true;
+  const isAuthenticated = false;
 
   return (
     <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={<PublicRoute isAuthenticated={isAuthenticated} />}
-        >
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Route>
-        <Route
-          path="/"
-          element={<PrivateRoute isAuthenticated={isAuthenticated} />}
-        >
-          {routes.map(({ component: Component, path }) => (
-            <Route path={`/${path}`} key={path} element={<Component />} />
-          ))}
-        </Route>
-        {/* <Route path="*" element={<NoFoundComponent />} /> */}
-        {/* <Route path="/settings" element={<Settings />} /> */}
-        {/* <Route path="/profile" element={<Profile />} /> */}
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route
+            path="/"
+            element={<PublicRoute isAuthenticated={isAuthenticated} />}
+          >
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+          </Route>
+          <Route
+            path="/"
+            element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+          >
+            {routes.map(({ component: Component, path }) => (
+              <Route path={`/${path}`} key={path} element={<Component />} />
+            ))}
+          </Route>
+          {/* <Route path="*" element={<NoFoundComponent />} /> */}
+          {/* <Route path="/settings" element={<Settings />} /> */}
+          {/* <Route path="/profile" element={<Profile />} /> */}
+        </Routes>
+      </Suspense>
     </Router>
   );
 }

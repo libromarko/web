ed12e94e627a3e155d06fa2f94d43f54c5e330ab
yplog <yalinpala@protonmail.com>
2022-09-27 14:58:47 +0300

    added feature component

---
+++ src/components/Feature/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+export default function Feature() {
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="center"
+      >a</Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >b</Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent="flex-end"
+        alignItems="center"
+      >c</Grid>
+    </>
+  );
+}
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Container, Box } from "@mui/system";
 import HeaderComponent from "../../components/Header";
 import StoresComponent from "../../components/Stores";
+import Feature from "../../components/Feature";
 
 export default function Landing() {
   return (
     <Container maxWidth={"xl"}>
       <Box sx={{ minHeight: "100vh" }}>
         <HeaderComponent />
+        <Feature />
         <StoresComponent />
       </Box>
     </Container>

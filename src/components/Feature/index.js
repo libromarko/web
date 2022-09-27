438ceb5856df91a861a921f915fb20e76c9b37f1
yplog <yalinpala@protonmail.com>
2022-09-27 15:27:32 +0300

    added FeatureCard component

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -1,40 +1,13 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import { CardActionArea } from "@mui/material";
+import FeatureCard from "../FeatureCard";
 
 export default function Feature() {
   return (
     <Box sx={{ flexGrow: 1, mt: 10, mb: 10 }}>
       <Grid container spacing={2} justifyContent="center" alignItems="center">
-        {Array.from(Array(3)).map((x, i) => (
-          <Grid item md>
-            <Card sx={{ maxWidth: 345 }} key={i}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image="https://via.placeholder.com/150"
-                  alt="green iguana"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Lizard
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
+        <FeatureCard />
       </Grid>
     </Box>
   );
